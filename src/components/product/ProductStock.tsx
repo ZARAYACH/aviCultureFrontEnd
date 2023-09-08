7a3f7a58f891c36988cfb1f89e1200330d31c9d9
MohammedChbani <mohammed.chbani@cprofile.nl>
2023-09-09 00:41:26 +0100

    a change

--- src/components/product/ProductStock.tsx
+++ src/components/product/ProductStock.tsx
@@ -58,8 +58,8 @@ const ProductStock = () => {
                               role="tab">
                             All Products
                         </Link>
-                        {products.map((product) => (
-                            <Link to={product.path} type="button"
+                        {products.map((product,index) => (
+                            <Link key={index} to={product.path} type="button"
                                   className="hs-tab-active:bg-white hs-tab-active:text-gray-700 hs-tab-active:dark:bg-gray-800 hs-tab-active:dark:text-gray-400 dark:hs-tab-active:bg-gray-800 py-3 px-4 inline-flex items-center gap-2 bg-transparent text-sm text-gray-500 hover:text-gray-700 font-medium rounded-md hover:hover:text-blue-600 dark:text-gray-400 dark:hover:text-white dark:hover:text-gray-300"
                                   role="tab">
                                 {product.name}
